be8b91faefa0b27ea1acf1f4543f452c4d0b8349
snowling444 <937035581@qq.com>
2017-03-23 10:31:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Installation
+
+```bash
+git clone https://github.com/snowling444/snow-3-23.git
+cd snow-3-23
+npm install
+npm start
+open http://localhost:3000
+```
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="jumbotron container-fluid">
+        <div className='row'>
+          <div className="col-xs-6 col-sm-4">Snow</div>
+          <div className="col-xs-6 col-sm-4">Milk</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
